dcba95d997e859e09614f0d937b8b13d53d8b92a
IbRuslan <ribragimov2003@gmail.com>
2023-07-30 15:12:06 +0500

    complete hw10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,7 @@
 import s from './Loader.module.css'
+import loader from '../../spinner.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}>
+    <img src={loader} alt="loading"/>
+</div>
+
