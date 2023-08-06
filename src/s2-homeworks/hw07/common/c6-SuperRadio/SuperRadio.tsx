21023fa2794788fb11a20a82198c73cba8680150
IbRuslan <ribragimov2003@gmail.com>
2023-08-06 20:21:58 +0500

    maybe fix hw7

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -52,7 +52,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       className={finalRadioClassName}
                       type={'radio'}
                       // name, checked, value делают студенты
-                      name={o.value}
+                      name={name}
                       checked={value === o.id}
                       value={o.id}
                       onChange={onChangeCallback}
